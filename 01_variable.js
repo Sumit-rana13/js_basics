a9f8bbf3941313a074e6d8554594a4f2c6fa468c
Sumit Rana <sr8218648@gmail.com>
2023-09-10 22:58:25 +0530

    01_variable.js and 02_datatype.js file added

---
+++ 01_variable.js
@@ -0,0 +1,13 @@
+const accountId =14412;
+let accountEmail = "sumit@gamil.com";
+var accountPassword = "1234";
+let accountState;
+
+// accountId = 2;  // not allowed
+
+console.log(accountId);
+/*
+    prefer not to use var
+    because of scope of block scope and function scope
+*/
+console.table([accountEmail,accountId,accountPassword,accountState]);
---
+++ 02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 +3) //we are using nodejs , browser
+
+// type of datatype
+// 1. number
+// 2.bigint
+// 3.string
+// 4.boolean => true/false
+// 5.null => standalone value
+// 6.undefined => value not assign
+// 7.symbol => unquie
+
+let age = 23;
+let name = "sumit";
+let state;
+
+// console.log([age,name,state]);
+
+console.log([typeof age,typeof name,typeof state]);
+
+console.log(typeof null);  // type of null is object  
